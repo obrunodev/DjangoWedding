0f77a86e4b9f5f01ef43b9c9be84d22a6c76902c
Bruno R. Pianca <brunorpdev@gmail.com>
2022-05-18 19:06:15 -0300

    Setting views and login page

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def register(request):
     if request.method == 'GET':
         return render(request, 'accounts/pages/register.html')
+
+
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'account/pages/login.html')
