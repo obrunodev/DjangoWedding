27a149a1929d1e93e941f4a8d653240de3531c81
Bruno R. Pianca <brunorpdev@gmail.com>
2022-06-20 01:58:32 -0300

    feat: CRUD (wip) - Create functin done

--- apps/posts/views/posts.py
+++ apps/posts/views/posts.py
@@ -1,12 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from ..models import Post
+from ..forms import PostForm
 
 
 def index(request):
-    return render(request, 'posts/pages/index.html')
+    posts = Post.objects.all()
+    context = {'posts': posts}
+    return render(request, 'posts/pages/index.html', context)
 
 
 def create(request):
-    pass
+    if request.method == 'POST':
+        form = PostForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+            return redirect('posts:index')
+        return redirect('posts:create')
+    else:
+        form = PostForm()
+        context = {'form': form}
+        return render(request, 'posts/pages/create.html', context)
 
 
 def update(request):
